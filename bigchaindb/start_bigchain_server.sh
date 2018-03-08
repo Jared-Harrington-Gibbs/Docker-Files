7d0da69d195d3e2d3a56118e603b685e7e2775d3
Jared Harrington-Gibbs <jhgworking@gmail.com>
2018-03-08 15:45:05 -0500

    Automated the start of the client
    Explanation:
    The starting of the client was automated.
    The name of the docker network was changed.
    
    Reason:
    This makes it easier to consistently launch
    the demo environment.
    
    Example cases:
    null
    
    Tags:
    [bigchaindb]

--- bigchaindb/start_bigchain_server.sh
+++ bigchaindb/start_bigchain_server.sh
@@ -1,33 +1,42 @@
+#create the mongodb user
 sudo useradd -r --uid 999 mongodb
 
+#make the bigchain storage directory
 mkdir $HOME/bigchain
 sudo rm $HOME/bigchain/* -rf
 cd $HOME/bigchain
 
-docker network create mynet
+#Create the bigchain network
+docker network create bigchainnet
 
+#Start the mongodb database
 docker run --name=mongodb \
   --detach \
-  --net mynet \
+  --net bigchainnet \
   --restart=always \
   --volume=$HOME/bigchain/mongodb_docker/db:/data/db \
   --volume=$HOME/bigchain/mongodb_docker/configdb:/data/configdb \
   mongo:3.4.9 --replSet=bigchain-rs
 
-mongodbIP=$(docker inspect --format={{.NetworkSettings.Networks.mynet.IPAddress}} mongodb)
+#Get the mongodb ip address
+mongodbIP=$(docker inspect --format={{.NetworkSettings.Networks.bigchainnet.IPAddress}} mongodb)
 
+#Generate the bigchain config
 docker run --interactive \
   --rm \
   --tty \
-  --net mynet \
+  --net bigchainnet \
   --volume $HOME/bigchain/bigchaindb_docker:/data \
   --env BIGCHAINDB_DATABASE_HOST=$mongodbIP bigchaindb/bigchaindb -y configure mongodb
 
+#Start the bigchain container
 docker run --name=bigchaindb \
   --detach \
-  --net mynet \
+  --net bigchainnet \
   --publish=59984:9984 \
   --restart=always \
   --volume=$HOME/bigchain/bigchaindb_docker:/data \
   bigchaindb/bigchaindb start
 
+#start the bigchain client
+docker run -i -t --rm --name=client --net bigchainnet jaredharringtongibbs/bigchaindb-client:latest
