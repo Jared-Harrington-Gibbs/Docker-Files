6771ec23e54493b24c22dcd08e1578a0cb9dcd75
nickscherr <nickscherr@yahoo.ca>
2018-03-12 13:07:04 -0400

    forgot to run git commit add .

--- bigchaindb/scripts/bigchain_transaction.py
+++ bigchaindb/scripts/bigchain_transaction.py
@@ -1,5 +1,11 @@
+# create an object class of BigchainDB
 from bigchaindb_driver import BigchainDB
+
+# Public and Private keypairs are generated for non-repudiation. 
+# Transactions are signed with private key
+# Public keys are to verify that the person in question truly did sign the transaction.
 from bigchaindb_driver.crypto import generate_keypair
+
 from time import sleep
 from sys import exit
 
@@ -7,11 +13,14 @@ from sys import exit
 alice, bob = generate_keypair(), generate_keypair()
 
 
-bdb_root_url = 'http://bigchaindb:9984/'  # Use YOUR BigchainDB Root URL here
+# Use YOUR BigchainDB Root URL here
+bdb_root_url = 'http://bigchaindb:9984/'
+
+# Our blockchain does not require authentication tokens  
 bdb = BigchainDB(bdb_root_url)
 
 
-# create asset (for example, a bicyclye), then assign it data and values
+# create digital asset payload {'data': {'data name': 'data value'}}
 bicycle_asset = {
     'data': {
         'bicycle': {
@@ -21,10 +30,12 @@ bicycle_asset = {
     },
 }
 
+# OPTIONAL - assign metadata to the asset
 bicycle_asset_metadata = {
     'planet': 'earth'
 }
 
+#The asset belongs to Alice and will be transferred to Bob
 prepared_creation_tx = bdb.transactions.prepare(
     operation='CREATE',
     signers=alice.public_key,
@@ -32,15 +43,18 @@ prepared_creation_tx = bdb.transactions.prepare(
     metadata=bicycle_asset_metadata
 )
 
+#The transaction will be fulfilled by signing it with Alice's private key
 fulfilled_creation_tx = bdb.transactions.fulfill(
     prepared_creation_tx,
     private_keys=alice.private_key
 )
 
+#A record of the transaction is sent over a BigchainDB node
 sent_creation_tx = bdb.transactions.send(fulfilled_creation_tx)
 
 txid = fulfilled_creation_tx['id']
 
+#code that checks the status of the transaction until it is successful
 trials = 0
 while trials < 60:
     try:
@@ -50,20 +64,19 @@ while trials < 60:
     except bigchaindb_driver.exceptions.NotFoundError:
         trials += 1
         sleep(1)
-
 if trials == 60:
     print('Tx is still being processed... Bye!')
     exit(0)
 
+# Find the id of the created asset to be transferred
 asset_id = txid
-
 transfer_asset = {
     'id': asset_id
 }
 
+# Prepare the transfer transaction
 output_index = 0
 output = fulfilled_creation_tx['outputs'][output_index]
-
 transfer_input = {
     'fulfillment': output['condition']['details'],
     'fulfills': {
@@ -72,7 +85,6 @@ transfer_input = {
     },
     'owners_before': output['public_keys']
 }
-
 prepared_transfer_tx = bdb.transactions.prepare(
     operation='TRANSFER',
     asset=transfer_asset,
@@ -80,11 +92,13 @@ prepared_transfer_tx = bdb.transactions.prepare(
     recipients=bob.public_key,
 )
 
+# Fulfill the transaction
 fulfilled_transfer_tx = bdb.transactions.fulfill(
     prepared_transfer_tx,
     private_keys=alice.private_key,
 )
 
+#Send record of transaction to BigchainDB node
 sent_transfer_tx = bdb.transactions.send(fulfilled_transfer_tx)
 
 print("Is Bob the owner?",
